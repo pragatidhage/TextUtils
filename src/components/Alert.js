25d72c071f61d9059d1c555a791f299f79859a50
Pragati Dhage <pragatidhage971@gmail.com>
2024-02-14 13:19:15 +0530

    fixed the alert component

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -4,7 +4,7 @@ function Alert(props) {
   return (
     <div style={{height : '60px'}}>
       {
-        props.alert && <div className="alert alert-warning alert-dismissible fade show" role="alert" style={{height : '45px'}}>
+        props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert" style={{height : '45px'}}>
         <strong>{props.alert.type}</strong>: {props.alert.msg}
         <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
